cba7b17516fa8fb85efd155ec6600dce6471172c
Josh Taylor <jtaylor4@bootcampspot.com>
2023-02-26 13:32:15 -0800

    added question form/prompts

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const inquirer = require('inquirer');
 const fs = require('fs');
 
 const generateHTML = () => {
@@ -27,12 +28,45 @@ const generateHTML = () => {
 }
 
 const init = () => {
-    const html = generateHTML();
+    inquirer.prompt([{
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?',
+    },
+    {
+        type: 'input',
+        name: 'location',
+        message: 'Where are you from?',
+    },
+    {
+        type: 'input',
+        name: 'hobby',
+        message: 'What is your favorite hobby?',
+    },
+    {
+        type: 'input',
+        name: 'food',
+        message: 'What is your favorite food?',
+    },
+    {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub Username',
+    },
+    {
+        type: 'input',
+        name: 'linkedin',
+        message: 'Enter your LinkedIn URL.',
+    }]).then((answers) => {
+        console.log(answers);
+    });
 
-    const fileName = 'index.html';
-    fs.writeFile(fileName, html, (err) => {
-        err ? console.log(err) : console.log(`Successfully created ${fileName}!`);
-    })
+    // const html = generateHTML();
+
+    // const fileName = 'index.html';
+    // fs.writeFile(fileName, html, (err) => {
+    //     err ? console.log(err) : console.log(`Successfully created ${fileName}!`);
+    // })
 }
 
 init();
